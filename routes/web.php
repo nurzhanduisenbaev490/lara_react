32a7c3c855af431b9998ee9f295179ecece33d6b
nurzhan <nurzhanduisenbaev480@mail.ru>
2026-06-01 21:16:14 +0500

    Post model, view, controller

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,15 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::get('/', function () {
-//    return view('app');
-//    return inertia('Home');
-//    sleep(2);
-    return Inertia::render('Home', ['name'=>'Moto']);
-});
+//Route::get('/', function () {
+////    return view('app');
+////    return inertia('Home');
+////    sleep(2);
+//    return Inertia::render('Home', ['name'=>'Moto']);
+//});
 
 //Route::inertia('/', 'Home2');
+Route::get('/', [PostController::class, 'index']);
+Route::resource('posts', PostController::class)->except('index');
